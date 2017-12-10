f81b894ea982f167cc2e58b58b70dc46ab114dad
GengShen <453992341@qq.com>
2017-12-10 22:53:31 +0800

    Update diag_main.py

--- diag_main.py
+++ diag_main.py
@@ -7,7 +7,7 @@ def qun_op_msg(msg):
     if msg["FromUserName"] == "@@f6c47f4349ae56e8abe9fe03ad0f195ab7ada89915a6279084f698d2612aa36a" and msg["IsAt"] == True:
         content = msg["Text"]
         if "的电话" in content:
-            name = content.split("沈耕")[-1].split("的")[0].lstrip()
+            name = content.split("myname")[-1].split("的")[0].lstrip() #myname is Nickname
             with open("diag",'r') as f:
                 md = [i.strip() for i in f.readlines()]
                 for i in md:
